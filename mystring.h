8ea59cfde0831288c4ab903266bf36a29b5eb9d5
SzekeresBarnabas <barnabasszekeress@gmail.com>
2024-10-18 16:01:01 +0200

    New mystring

--- mystring.h
+++ mystring.h
@@ -1,20 +1,22 @@
 //#include<>
-class MyString
+class MyStringValue
 {
     char* s_;
     int size_;
 
 public:
-    MyString(char* s, int size): size_{size}
+    MyStringValue(char* s, int size): size_{size}
     {
         this->s_ = new char(size+1);
         for(int i = 0; i <= size; i++)
         {
             this->s_[i] = s[i];
         }
+
+        this->s_[size+1] = '\0';
     }
 
-    ~MyString()
+    ~MyStringValue()
     {
         delete[] this->s_;
     }
@@ -22,5 +24,27 @@ public:
     char* getChar()
     {
         return this->s_;
+    }
+};
+class MyString
+{
+    MyStringValue* str_;
+    int* prefCount;
+public:
+    MyString(MyStringValue* str): str_{str}
+    {
+        if(str)
+        {
+            prefCount = new int{1};
+        }
+        else
+        {
+            prefCount = nullptr;
+        }
+
+    }
+    MyString(MyString& theOther):
+    {
+
     }
 };
--- test.cpp
+++ test.cpp
@@ -3,10 +3,12 @@
 
 int main()
 {
-    MyString alma = MyString("alma", 4);
-    char *text = alma.getChar();
-    
-    std::cout<< text <<std::endl;
+    //MyString alma = MyString("alma", 4);
+    //char *text = alma.getChar();
+
+    //std::cout<< text <<std::endl;
+
+    std::cout<< "Hello World!" <<std::endl;
 
     return 0;
 }
